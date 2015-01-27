47cfee83be6cbff58f007b36afd290c777598bce
Liwer <liwermon@gmail.com>
2015-01-27 13:55:43 +0200

    fixed error in articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ end
   def new
     @article = Article.new
   end
-end
+
 def create
 @article = Article.new(article_params)
 
@@ -17,7 +17,7 @@ if @article.save
 redirect_to @article
 else render 'new'
 end
-
+end
 private
 def article_params
   params.require(:article).permit(:title, :text)
